e33b05d5c660f76d1fb4b32a5a8c6b2efbe93985
Raissa Correa <raissacorreaa@gmail.com>
2020-10-16 13:52:33 -0300

    day03

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,41 +1,38 @@
 const options = {
-    dragging: false,
-    touchZoom: false,
-    doubleClickZoom: false,
-    scrollWheelZoom: false,
-    zoomControl: false,
-}
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
+};
 
-const map = L.map('mapid', options).setView([-19.9192333,-43.9386843], 15);
+const map = L.map("mapid", options).setView([-19.9192333, -43.9386843], 15);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',)
-.addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
-    iconSize: [58, 68],
-    iconAnchor: [29,68],
-    popAnchor: [170, 2]
-})
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popAnchor: [170, 2],
+});
 
-L
-.marker([-19.9192333,-43.9386843], { icon })
-.addTo(map)
+L.marker([-19.9192333, -43.9386843], { icon }).addTo(map);
 
 function selectImage(event) {
-    const button = event.currentTarget;
+  const button = event.currentTarget;
 
-    const buttons = document.querySelectorAll(".images button")
-    buttons.forEach(removeActiveClass);
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach(removeActiveClass);
 
-    function removeActiveClass(button) {
-        button.classList.remove('active');
-    }
+  function removeActiveClass(button) {
+    button.classList.remove("active");
+  }
 
-    const image = button.children[0];
-    const imageContainer = document.querySelector('.orphanage-details > img');
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
 
-    imageContainer.src = image.src;
+  imageContainer.src = image.src;
 
-    button.classList.add('active');
+  button.classList.add("active");
 }
